cd6d6a379d38cd82e0cdad8c1091c2ae282e94b5
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2013-03-22 00:25:27 +0100

    [models] Field uses an enum to get the field type

--- src/models/Field.py
+++ src/models/Field.py
@@ -1,5 +1,9 @@
+from ..utils.enum import Enum
+
+Surface = Enum(["SAND", "GRASS", "CONCRETE"]) 
+
 class Field(object):
-    def __init__(self, _width, _height, _surface):
+    def __init__(self, _width, _height, _surface=Surface.SAND):
         self.width = _width
         self.height = _height
         self.surface = _surface
