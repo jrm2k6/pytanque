93f2a6d97123fea75bcad387fdc19b7044f65b63
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2013-03-22 00:37:35 +0100

    [main] use pygame to display main window

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from models.Player import Player
-from models.Team import Team
-from views.MainWindow import MainWindow
-import gtk
+import sys
+
+import pygame
+from pygame.locals import QUIT
 
 if __name__ == '__main__':
-    MainWindow()
-    gtk.main()
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((900, 800))
+    pygame.display.set_caption('pytanque')
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                pygame.display.update()
+    
     
     
     
